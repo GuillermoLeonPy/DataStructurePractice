4a7ddfcd4cf740f772b8bd8e79e2ac9a70220c39
Guillermo Leon <guillermo.leon.py@gmail.com>
2023-12-13 08:12:53 -0300

    Data structure parser version 2 refinement

--- src/com/softdev/gl/MyDataStructureParser.java
+++ src/com/softdev/gl/MyDataStructureParser.java
@@ -9,73 +9,119 @@ import java.util.Map;
 
 public class MyDataStructureParser {
 
-	private static final String TEST_DATA = "items:[id:PLAN INTERNET FIJO|quantity:1|producttype:Promotion|productcharacteristic:[name:service-type|value:Promotion,name:siebel-price-list|value:LP HOME],id:Servicio de Internet|quantity:1|producttype:Product|productcharacteristic:[name:service-type|value:Agrupador],id:Equipo Internet|quantity:1|producttype:Product]";
-	private static final String TEST_DATA_2 = "p1:AAA|p2:BBB|p3:[p1:CC|p2:DDD|p3:[p1:EEE|p2:FFF|p3:[p1:GGG|p2:HHH,p1:III|p2:JJJ],p1:KKK|p2:LLL],p1:MMM|p2:NNN],p1:OOO|p2:PPP";
-	private static final String TEST_DATA_3 = "p1:AAA|p2:BBB|p3:[p1:CC|p2:DDD|p3:[p1:EEE|p2:FFF|p3:[p1:GGG|p2:HHH,p1:III|p2:JJJ],p1:KKK|p2:LLL|p3:[p1:TTT|p2:UUU]],p1:MMM|p2:NNN],p1:OOO|p2:PPP|p3:[p1:QQQ|p2:RRR]";
-	private static final String TEST_DATA_4 = "p1:AAA|p2:BBB|p3:[p1:CC|p2:DDD|p3:[p1:EEE|p2:FFF|p3:[p1:GGG|p2:HHH,p1:III|p2:JJJ],p1:KKK|p2:LLL|p3:[p1:TTT|p2:UUU]],p1:MMM|p2:NNN],p1:OOO|p2:PPP|p3:[p1:QQQ|p2:RRR],p1:WWW|p2:XXX";
+	private static final String TEST_DATA = "p1:AAA|p2:BBB|p3:[p1:CC|p2:DDD|p3:[p1:EEE|p2:FFF|p3:[p1:GGG|p2:HHH,p1:III|p2:JJJ],p1:KKK|p2:LLL|p3:[p1:TTT|p2:UUU]],p1:MMM|p2:NNN],p1:OOO|p2:PPP|p3:[p1:QQQ|p2:RRR],p1:WWW|p2:XXX";
 	
-	private static MyDataStructureParser parser = new MyDataStructureParser();
+	private static final MyDataStructureParser parser = new MyDataStructureParser();
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Map<Integer,List<Integer[]>> beginEndSquareBracketStartEndIndexesPerLevel = parser.recordBeginEndSquareBracketStartEndIndexesPerLevel(TEST_DATA_4);
-		List<Integer[]> beginEndObjectsIndexes = parser.getBeginEndObjectIndexPerLevel(beginEndSquareBracketStartEndIndexesPerLevel, 1, TEST_DATA_4);
-		for(Integer[] pair : beginEndObjectsIndexes) {
-			System.out.println(TEST_DATA_4.substring(pair[0], pair[1]));
+		//parser.testGetObjectIndexes();
+		parser.getObjectListTest();
+	}
+	
+	private void getObjectListTest() {
+		List<Map<String,Object>> objectList = parser.getObjectList(TEST_DATA, 0, TEST_DATA.length() - 1);
+		System.out.println(objectList);
+	}
+	
+	private List<Map<String,Object>> getObjectList(String rawData, int beginIndex, int endIndex) {
+		List<Integer[]> objectsIndexes = getObjectIndexes(rawData, beginIndex, endIndex);
+		List<Map<String,Object>> objectList = new ArrayList<>();
+		for(Integer[] beginEndObjectIndex : objectsIndexes) {
+			objectList.add(getObject(rawData, beginEndObjectIndex[0], beginEndObjectIndex[1]));
+		}
+		return objectList;
+	}
+	
+	private Map<String,Object> getObject(String rawData, int beginIndex, int endIndex){
+		Map<String,Object> object = new HashMap<>();
+		char[] charArray = rawData.substring(beginIndex, endIndex).toCharArray();
+		String propertyKey = "";
+		String propertyValue = "";
+		boolean keyInProgress = true;
+		int i = 0;
+		while(i < charArray.length) {			
+			if(charArray[i] == ':') {				
+				keyInProgress = false;
+			}else if(charArray[i] == '|') {				
+				keyInProgress = true;
+				object.put(propertyKey, propertyValue);
+				propertyKey = "";
+				propertyValue = "";
+			}else if(charArray[i] == '[') {
+				int listContentBeginIndex = beginIndex + i + 1;
+				int endListIndex = getEndListIndex(rawData, listContentBeginIndex - 1);
+				List<Map<String,Object>> objectList = getObjectList(rawData, listContentBeginIndex, endListIndex);
+				object.put(propertyKey, objectList);
+				propertyKey = "";
+				i = endListIndex;
+			}else if (keyInProgress) {
+				propertyKey+=charArray[i];
+			}else {
+				propertyValue+=charArray[i];
+			}
+			i++;
+		}
+		if(propertyKey.length() > 0 && propertyValue.length() > 0) {
+			object.put(propertyKey, propertyValue);
+		}
+		return object;
+	}
+	
+	private int getEndListIndex(String rawData, int beginIndex) {
+		char[] charArray = rawData.toCharArray();
+		Deque<Integer> stack = new ArrayDeque<Integer>();
+		int i = beginIndex;
+		while(i < charArray.length) {
+			if(charArray[i] == '[') {
+				stack.push(i);				
+			}else if(charArray[i] == ']') {				
+				stack.pop();
+			}
+			//
+			if(stack.size() == 0) {
+				break;
+			}
+			i++;
+		}
+		return i;		
+	}
+	
+	/** ################################# ################################# */
+	/** ################################# ################################# */
+	
+	private void testGetObjectIndexes() {
+		List<Integer[]> objectIndexes = parser.getObjectIndexes(TEST_DATA, 0, TEST_DATA.length() - 1);
+		for(Integer[] objectBeginEndIndexes : objectIndexes) {
+			System.out.println(objectBeginEndIndexes[0] + ", " +  objectBeginEndIndexes[1]);
+			System.out.println(TEST_DATA.substring(objectBeginEndIndexes[0], objectBeginEndIndexes[1]));
 		}
 	}
-
-	private Map<Integer,List<Integer[]>> recordBeginEndSquareBracketStartEndIndexesPerLevel(String rawData) {
-		char [] charArray = rawData.toCharArray();
+	
+	private List<Integer[]> getObjectIndexes(String rawData, int beginSearchIndex, int endSearchIndex){
 		Deque<Integer> stack = new ArrayDeque<Integer>();
-		Map<Integer,List<Integer[]>> beginEndSquareBracketStartEndIndexesPerLevel = new HashMap<>();
-		
+		String subString = rawData.substring(beginSearchIndex, endSearchIndex);
+		char[] charArray = subString.toCharArray();
+		List<Integer[]> objectListBeginEndIndexes = new ArrayList<>();
+		int beginObjectIndex = beginSearchIndex;
+		boolean checkCommaCharacter = true;
+		int currentObjectCharactersLength = 0;
 		for(int i = 0; i < charArray.length; i++) {
+			currentObjectCharactersLength++;
 			if(charArray[i] == '[') {
 				stack.push(i);
-			}else if (charArray[i] == ']') {
-				//squareBracketsBeginEndIndexes.add(new Integer [] {stack.pop(),i});
-				if(beginEndSquareBracketStartEndIndexesPerLevel.containsKey(stack.size())) {
-					beginEndSquareBracketStartEndIndexesPerLevel.get(stack.size()).add(new Integer [] {stack.pop(),i});
-				}else {
-					int newLevel = stack.size();
-					List<Integer[]> newLevelList = new ArrayList<>();
-					newLevelList.add(new Integer [] {stack.pop(),i});					
-					beginEndSquareBracketStartEndIndexesPerLevel.put(newLevel, newLevelList);					
-				}				
-			}
-		}
-		return beginEndSquareBracketStartEndIndexesPerLevel;
-	}
-	
-	private List<Integer[]> getBeginEndObjectIndexPerLevel(Map<Integer,List<Integer[]>> beginEndSquareBracketStartEndIndexesPerLevel, int level, String rawData){
-		List<Integer[]> beginEndSquareBracketStartEndIndexes = beginEndSquareBracketStartEndIndexesPerLevel.get(level);
-		int beginObjectIndex = 0;
-		List<Integer[]> beginEndObjectIndexPerLevel = new ArrayList<>();
-		for(int i = 0; i < beginEndSquareBracketStartEndIndexes.size(); i++) {			
-			int subStringBeginIndex = beginEndSquareBracketStartEndIndexes.get(i)[1];
-			int subStringEndIndex = 0;
-			if(i == beginEndSquareBracketStartEndIndexes.size() - 1) {			
-				subStringEndIndex = rawData.length() - 1;
-			}else {
-				subStringEndIndex = beginEndSquareBracketStartEndIndexes.get(i + 1)[0];
-			}
-			char [] charArray = rawData.substring(subStringBeginIndex, subStringEndIndex).toCharArray();
-			for(int i1 = 0; i1 < charArray.length; i1++) {
-				if(charArray[i1] == ',') {
-					int endObjectIndex = subStringBeginIndex + i1;
-					beginEndObjectIndexPerLevel.add(new Integer [] {beginObjectIndex, endObjectIndex});
-					System.out.println(rawData.substring(beginObjectIndex, endObjectIndex));
-					System.out.println(rawData.substring(beginObjectIndex, endObjectIndex).length());
-					beginObjectIndex = endObjectIndex + 1;
+				checkCommaCharacter = false;
+			}else if(charArray[i] == ']') {				
+				stack.pop();
+				if(stack.size() == 0){
+					checkCommaCharacter = true;
 				}
-			}
-		}
-		if(beginObjectIndex < rawData.length()) {
-			beginEndObjectIndexPerLevel.add(new Integer [] {beginObjectIndex, rawData.length() - 1});
-		}
-		
-		return beginEndObjectIndexPerLevel;
+			}else if(checkCommaCharacter && charArray[i] == ',') {				
+				objectListBeginEndIndexes.add(new Integer[] {beginObjectIndex, beginObjectIndex + currentObjectCharactersLength - 1});
+				beginObjectIndex = beginObjectIndex + currentObjectCharactersLength;
+				currentObjectCharactersLength = 0;
+			}			
+		}		
+		objectListBeginEndIndexes.add(new Integer[] {beginObjectIndex, beginObjectIndex + currentObjectCharactersLength});
+		return objectListBeginEndIndexes;
 	}
-	
 }
